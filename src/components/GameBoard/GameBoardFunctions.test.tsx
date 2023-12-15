128f3ed14652cca85bdb1c1989ce29d50cb37ea2
Nolan <nolansingroy@gmail.com>
2023-12-15 15:17:16 -0800

    testing: playerScores fn

--- src/components/GameBoard/GameBoardFunctions.test.tsx
+++ src/components/GameBoard/GameBoardFunctions.test.tsx
@@ -1,4 +1,6 @@
 import { areAllShipsSunk } from '../GameBoard/GameBoard';
+import  { playerScores } from '../GameBoard/GameBoard';
+
 
 describe('areAllShipsSunk', () => {
   it('returns true when all ships are sunk', () => {
@@ -23,3 +25,37 @@ describe('areAllShipsSunk', () => {
     expect(areAllShipsSunk(grid)).toBe(false);
   });
 });
+
+
+
+//// playerScore function 
+
+describe('playerScores', () => {
+    it('increments score and plays hit sound if score is less than 15', () => {
+      const setPlayerScore = jest.fn();
+      const playHitSound = jest.fn();
+      const setGamePhase = jest.fn();
+      const triggerConfetti = jest.fn();
+  
+      playerScores(10, setPlayerScore, setGamePhase, playHitSound, triggerConfetti);
+  
+      expect(setPlayerScore).toHaveBeenCalledWith(expect.any(Function));
+      expect(playHitSound).toHaveBeenCalled();
+      expect(setGamePhase).not.toHaveBeenCalled();
+      expect(triggerConfetti).not.toHaveBeenCalled();
+    });
+  
+    it('sets game phase to gameOver and triggers confetti if score is 15', () => {
+      const setPlayerScore = jest.fn();
+      const playHitSound = jest.fn();
+      const setGamePhase = jest.fn();
+      const triggerConfetti = jest.fn();
+  
+      playerScores(15, setPlayerScore, setGamePhase, playHitSound, triggerConfetti);
+  
+      expect(setPlayerScore).not.toHaveBeenCalled();
+      expect(playHitSound).not.toHaveBeenCalled();
+      expect(setGamePhase).toHaveBeenCalledWith('gameOver');
+      expect(triggerConfetti).toHaveBeenCalled();
+    });
+  });
